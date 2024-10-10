2ceb6f33d4aab669186ac8963433d7390ff498f6
af3d3z <alonso.fernandez@iesnervion.es>
2024-10-10 12:43:09 +0200

    Api

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from flask import *
-import pymssql
 from models.Editorial import Editorial
 
-conn = pymssql.connect(
-    server='warmachine',
-    user='alebozek',
-    password='EjercicioAPI',
-    database='EjercicioAPI'
-)
+editoriales = [
+    {"id":1, "cif":123456789, "razon":"Manolo Books", "web":"https://manolobooks.com", "correo": "info@manolobooks.com", "tlf": 688641357},
+    {"id":2, "cif":987654321, "razon":"Planeta", "web":"https://planeta.com", "correo": "info@planeta.com", "tlf": 688641257}
+]
 
 app = Flask(__name__)
 
@@ -17,46 +14,44 @@ def index():
 
 @app.get('/editoriales')
 def get_editoriales():
-    editoriales = []
-    cursor.execute("SELECT * FROM EDITORIAL")
-    results = cursor.fetchall()
-    for r in results:
-        editorial = Editorial(id=r['Id'], cif=r['CIF'], razon=r['RazonSocial'], direccion=r['Direccion'], web=r['Web'], correo=r['Correo'], tlf=r['Telefono'])
-        editoriales.append(editorial.serialize())
     return jsonify(editoriales)
+
+@app.get('/editoriales/<int:id>')
+def get_editorial(id):
+    for n in editoriales:
+        if n['id'] == id:
+            return jsonify(n)
+
+    return jsonify({'error':'not found'})
 
 @app.post('/editoriales')
 def add_editorial():
     if request.is_json:
         editorial_json = request.get_json()
         editorial = Editorial(id=editorial_json['id'], cif=editorial_json['cif'], razon=editorial_json['razon'], direccion=editorial_json['direccion'], web=editorial_json['web'], correo=editorial_json['correo'], tlf=editorial_json['tlf'])
-        print(f"INSERT INTO EDITORIAL VALUES ({editorial.id}, {editorial.cif}, {editorial.razon}, {editorial.direccion}, {editorial.web}, {editorial.correo}, {editorial.tlf})")
-        cursor.execute(f"INSERT INTO EDITORIAL VALUES ({editorial.id}, {editorial.cif}, '{editorial.razon}', '{editorial.direccion}', '{editorial.web}', '{editorial.correo}', '{editorial.tlf}')")
-        return '{"msg": "El recurso ha sido creado."}',201
+        editoriales.append(editorial.serialize())
+        return editorial.serialize(),201
     else:
         return '{error: "JSON invalido"}', 501
 
-@app.put('/editoriales')
-@app.patch('/editoriales')
-def modify_editorial():
+@app.put('/editoriales/<int>:id')
+@app.patch('/editoriales/<int>:id')
+def modify_editorial(id):
     if request.is_json:
         editorial_json = request.get_json()
         new_editorial = Editorial(id=editorial_json['id'], cif=editorial_json['cif'], razon=editorial_json['razon'], direccion=editorial_json['direccion'], web=editorial_json['web'], correo=editorial_json['correo'], tlf=editorial_json['tlf'])
-        print(new_editorial)
-        cursor.execute(f"SELECT COUNT(1) FROM EDITORIAL WHERE ID = {new_editorial.id}")
-        result = cursor.fetchall()[0][0]
-        print(result)
-        if result == 1:
-            return "bomba"
-        else:
-            return {"error": "La editorial no existe."}  
+        for editorial in editoriales:
+            if editorial['id'] == id:
+                for element in new_editorial:
+                    editorial[element] = new_editorial[element]
+                return jsonify(new_editorial.serialize())
+
     else:
         return {"error": "JSON invalido"}
         
-        
+
         
         
 
 if __name__ == '__main__':
-    cursor = conn.cursor()
     app.run(debug=True, host="0.0.0.0", port="5050")
